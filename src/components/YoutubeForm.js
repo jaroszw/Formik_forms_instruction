ef4cc7d9b42be4439aa95da65e20b89f20757ded
Wojciech Jarosz <jaroszw@gmail.com>
2021-01-08 11:56:44 +0100

    up to FieldArray to add fields dinamicaly

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 import * as Yup from 'yup';
+
+import { TextError } from './TextError';
 
 const initialValues = {
   name: '',
   email: '',
-  channel: '',
+  // channel: '',
+  // comments: '',
+  // address: '',
+  // social: {
+  //   facebook: '',
+  //   twitter: '',
+  // },
+  // phoneNumbers: ['', ''],
+  phNumbers: [''],
 };
 
 const onSubmit = (values) => {
@@ -15,7 +25,8 @@ const onSubmit = (values) => {
 const validationSchema = Yup.object({
   name: Yup.string().required('Required'),
   email: Yup.string().email('Invalid email format').required('Required'),
-  channel: Yup.string().required('Required'),
+  // channel: Yup.string().required('Required'),
+  // address: Yup.string().required('Required'),
 });
 
 const YoutubeForm = () => {
@@ -28,20 +39,99 @@ const YoutubeForm = () => {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <Field type="text" name="name" id="name" />
-          <ErrorMessage name="name" />
+          <Field name="name" id="name" type="text" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
+
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="email" name="email" id="email" />
-          <ErrorMessage name="email" />
-        </div>
-        <div className="form-control">
-          <label htmlFor="channel">Channel</label>
-          <Field type="text" name="channel" id="channel" />
-          <ErrorMessage name="channel" />
+          <ErrorMessage name="email">
+            {(props) => {
+              console.log(props);
+              return <div className="error">{props}</div>;
+            }}
+          </ErrorMessage>
         </div>
 
+        {/* 
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <Field
+            type="text"
+            name="channel"
+            id="channel"
+            placeholder="Youtube Channel Name"
+          />
+          <ErrorMessage name="channel" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field name="comments" id="comments" as="textarea" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, form, meta } = props;
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
+        </div>
+
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <Field type="text" id="twitter" name="social.twitter" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="phoneNumbers">Phone 1</label>
+          <Field type="text" name="phoneNumbers[0]" id="phoneNumbers" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="phoneNumbers">Phone 2</label>
+          <Field type="text" id="phoneNumbers" name="phoneNumbers[1]" />
+          </div> */}
+
+        <div className="from-control">
+          <label>List of phone numbers</label>
+          <FieldArray name="phNumbers">
+            {(fieldArrayProps) => {
+              const { push, remove, form } = fieldArrayProps;
+              const { values } = form;
+              const { phNumbers } = values;
+
+              return (
+                <div className="from-control">
+                  {phNumbers.map((phNumber, index) => (
+                    <div key={index}>
+                      <Field type="text" name={`phNumbers[${index}]`} />
+                      <button type="button" onClick={() => remove(index)}>
+                        -
+                      </button>
+                      <button type="button" onClick={() => push('')}>
+                        +
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
+        </div>
         <button type="submit">Submit</button>
       </Form>
     </Formik>
